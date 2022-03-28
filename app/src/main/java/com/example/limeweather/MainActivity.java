57045873eba18a918732970879b27533c54bed45
EmilLimeOmnicron <EmilLimeOmnicron@users.noreply.github.com>
2022-03-27 20:06:16 -0700

    notes added, need to use URL and get JSON

--- app/src/main/java/com/example/limeweather/MainActivity.java
+++ app/src/main/java/com/example/limeweather/MainActivity.java
@@ -9,6 +9,17 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
 
 /*
 * step 1: enter city and country
@@ -18,6 +29,7 @@ import android.widget.TextView;
 * task: required on screen, input for city, country
 * view for weather
 * show toast if error inputting city/country/etc
+* method to get json object and parse the json
 *
 * */
 
@@ -28,11 +40,13 @@ import android.widget.TextView;
 //https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
 //example: https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={API key}
 public class MainActivity extends AppCompatActivity {
-    //private static final String url =
+   //"https://api.openweathermap.org/data/2.5/weather?q="
+    JSONObject jsonObject = new JSONObject();
     EditText editCity, editCountry;
     TextView result;
 
-    //JSONObject json =
+    //use URL to get json
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,4 +54,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    public String addLocationToURL(String url) {
+return url;
+    }
+
 }
